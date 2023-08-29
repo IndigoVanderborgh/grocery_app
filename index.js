954dd75d507126c8a29883704475898b0de52f4c
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-08-28 23:09:22 -0700

    1

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-2d26c-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListinDB = ref(database, "shoppingList")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
+
+// Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
+    push(shoppingListInDB, inputValue)
     console.log(inputValue)
 })
+
+
+
+
+
+// call your project: realtime database
+// disable google analytics
+// create a realtime database
+//  set the realtime database location to nearest you
+// set the security rules to test mode
+// copy realtime database reference URL
