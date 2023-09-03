503d8df338af5df582ad28740f0886b099dcc3f2
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-09-02 17:48:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const appSettings = {
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const shoppingListinDB = ref(database, "shoppingList")
+const shoppingListInDB = ref(database, "shoppingList")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
